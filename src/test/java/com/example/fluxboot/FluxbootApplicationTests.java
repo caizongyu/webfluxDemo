f63280a94991d0184fa023d4f7ef827a37572b5a
caizongyu <876570244@qq.com>
2022-07-19 11:27:51 +0800

    webclient 测试类

--- src/test/java/com/example/fluxboot/FluxbootApplicationTests.java
+++ src/test/java/com/example/fluxboot/FluxbootApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.fluxboot;
 
+import com.example.fluxboot.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class FluxbootApplicationTests {
 
     @Test
     void contextLoads() {
+        WebClient webClient = WebClient.create("http://localhost:8080");
+        Mono<User> resp = webClient.get().uri("/flux/user").retrieve().bodyToMono(User.class);
+        System.out.println(resp.block());
     }
 
 }
